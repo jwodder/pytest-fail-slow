3e4c7a09d1d35e64b6100e2466f4276b6496b8df
John T. Wodder II <git@varonathe.org>
2021-12-10 09:53:20 -0500

    Test presence of "Test took too long to run" message

--- test/test_fail_slow.py
+++ test/test_fail_slow.py
@@ -2,11 +2,12 @@ import pytest
 
 
 @pytest.mark.parametrize(
-    "src,success",
+    "src,success,slow",
     [
         (
             "def test_func():\n    assert 2 + 2 == 4\n",
             True,
+            False,
         ),
         (
             "from time import sleep\n"
@@ -14,9 +15,11 @@ import pytest
             "    sleep(10)\n"
             "    assert 2 + 2 == 4\n",
             False,
+            True,
         ),
         (
             "def test_func():\n    assert 2 + 2 == 5\n",
+            False,
             False,
         ),
         (
@@ -25,13 +28,18 @@ import pytest
             "    sleep(10)\n"
             "    assert 2 + 2 == 5\n",
             False,
+            False,
         ),
     ],
 )
-def test_fail_slow(pytester, src: str, success: bool) -> None:
+def test_fail_slow(pytester, src: str, success: bool, slow: bool) -> None:
     pytester.makepyfile(test_func=src)
     result = pytester.runpytest("--fail-slow=5")
     if success:
         result.assert_outcomes(passed=1)
     else:
         result.assert_outcomes(failed=1)
+    if slow:
+        result.stdout.fnmatch_lines(["Test took too long to run: Duration *s > 5*s"])
+    else:
+        result.stdout.no_fnmatch_line("Test took too long to run: Duration *s > 5*s")
